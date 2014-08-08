982a4c43974be3ac48e6873cdf1419c621e67be6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-08-08 17:51:39 +0530

    update README

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Segment.io support for Meteor (works on both client and server)"
+  summary: "Segment.io integration for Meteor (works on both client and server)"
 });
 
 Npm.depends({
@@ -10,5 +10,5 @@ Package.on_use(function (api) {
   api.add_files('snippet.js', 'client');
   api.add_files('server.js', 'server');
 
-  api.export(['analytics']);
+  api.export('analytics');
 });
--- server.js
+++ server.js
@@ -2,5 +2,11 @@ var Analytics = Npm.require('analytics-node');
 
 analytics = {};
 analytics.load = function(writeKet) {
-  analytics = new Analytics(writeKet);
+  var a = new Analytics(writeKet);
+
+  // extending a's functionality to the analytics namespace
+  analytics.__proto__ = a.__proto__;
+  _.each(a, function(value, key) {
+    analytics[key] = value;
+  });
 };
