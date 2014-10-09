d33e595fef20fd633003f17b6b835de5a7c2318e
schnie <greg@differential.io>
2014-10-09 11:33:35 -0400

    add server.js to server

--- package.js
+++ package.js
@@ -11,6 +11,7 @@ Npm.depends({
 
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@0.9.1.1');
-  api.add_files('snippet.js', 'client');
+  api.addFiles('snippet.js', 'client');
+  api.addFiles('server.js', 'server');
   api.export('analytics');
 });
