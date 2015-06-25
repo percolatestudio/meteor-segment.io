12f4907da061a0f966585a02c5b53b39c7c391d7
Zoltan Olah <zol@percolatestudio.com>
2015-06-25 13:02:20 -0700

    Bumps version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Segment.io integration for Meteor (works on both client and server)",
-  version: "1.1.1_1",
+  version: "1.2.2_1",
   name: "percolatestudio:segment.io",
   git: "https://github.com/percolatestudio/meteor-segment.io.git"
 });
